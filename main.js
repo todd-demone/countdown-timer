8ca77cfe660337d49c96acd86fc04441d7787a7a
Todd Demone <tdemone1@gmail.com>
2020-03-24 13:47:41 -0400

    added pause functionality

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 // By Todd Demone
 // last updated: 2020-03-24
 
-// Access the relevant HTML elements
 const startButton = document.getElementById('startButton');
 const hoursFace = document.getElementById('hoursFace');
 const minutesFace = document.getElementById('minutesFace');
@@ -15,27 +14,31 @@ const seconds = parseInt(prompt('How many SECONDS do you want to add to the time
 
 let totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
 let hoursRemaining, minutesRemaining, secondsRemaining;
+let invokeInterval = null;
 
 // Set the initial start time on the clock 
 calculateTimeRemaining();
-// const hoursStart = Math.floor( totalSeconds / 3600 );
-// const minutesStart = Math.floor( (totalSeconds % 3600) / 60 );
-// const secondsStart = ( totalSeconds % 3600 ) % 60 ;
-// update the View 
 drawClockFace();
-// hoursFace.innerHTML = `${hoursStart < 10 ? '0' : ''}${hoursStart}`;
-// minutesFace.innerHTML = `${minutesStart < 10 ? '0' : ''}${minutesStart}`;
-// secondsFace.innerHTML = `${secondsStart < 10 ? '0' : ''}${secondsStart}`;
 
 // Click the start button to start the countdown
 startButton.addEventListener( 'click', startTimer );
 
 function startTimer() {
-  startButton.removeEventListener( 'click', startTimer ); // disable the "Click to Start" button so multiple clicks don't speed up the timer!
-  const invokeInterval = window.setInterval( tick, 1000 ); // run the tick() function every 1000 milliseconds
+  // REFACTOR
+  if (!invokeInterval) {
+    invokeInterval = window.setInterval( tick, 1000 );
+    startButton.innerHTML = 'Pause';
+  } else {
+    window.clearInterval(invokeInterval);
+    invokeInterval = null;
+    startButton.innerHTML = 'Start';
+  }
+  // END OF REFACTOR
+  // startButton.removeEventListener( 'click', startTimer ); // disable the "Click to Start" button so multiple clicks don't speed up the timer!
+  // const invokeInterval = window.setInterval( tick, 1000 ); // run the tick() function every 1000 milliseconds
   
   function tick() {
-    if (totalSeconds < 0) {
+    if (totalSeconds === 0) {
       endTimer();
     } else {
       removeOneSecond();
@@ -52,21 +55,9 @@ function startTimer() {
   function removeOneSecond() {
     totalSeconds--;
     calculateTimeRemaining();
-    // const hoursRemaining = Math.floor( totalSeconds / 3600 );
-    // const minutesRemaining = Math.floor( (totalSeconds % 3600) / 60 );
-    // const secondsRemaining = ( totalSeconds % 3600 ) % 60 ;
-    // View
     drawClockFace();
-    // hoursFace.innerHTML = `${hoursRemaining < 10 ? '0' : ''}${hoursRemaining}`;
-    // minutesFace.innerHTML = `${minutesRemaining < 10 ? '0' : ''}${minutesRemaining}`;
-    // secondsFace.innerHTML = `${secondsRemaining < 10 ? '0' : ''}${secondsRemaining}`;
   }
 }
-// REFACTOR
-
-// let hoursRemaining, minutesRemaining, secondsRemaining;
-// calculateTimeRemaining();
-// drawClockFace();
 
 function calculateTimeRemaining() {
   hoursRemaining = Math.floor( totalSeconds / 3600 );
