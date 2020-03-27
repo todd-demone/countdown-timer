548c0a18808f1dfece87f9963f2ddd045e2b6794
Todd Demone <tdemone1@gmail.com>
2020-03-27 12:13:50 -0400

    refactored code

--- main.js
+++ main.js
@@ -1,32 +1,54 @@
 // Countdown Timer
 // By Todd Demone
-// last updated: 2020-03-24
+// last updated: 2020-03-27
 
+// Global variables 
+let clockArray = [0,0,0,0,0,0];
+let timeStr = `00h00m00s`;
+let totalSecs, hrs, mins, secs;
+let counter = 1; // for timerInput()
+let invokeInterval = null; // where window.setInterval() is stored. It's global so I can toggle between 'Start' and 'Pause'
+const clockFace = document.getElementById("clockFace");
+const keys = document.querySelectorAll("#keyPad > .row > .key");
 const startButton = document.getElementById('startButton');
 const resetButton = document.getElementById('resetButton');
-const hoursFace = document.getElementById('hoursFace');
-const minutesFace = document.getElementById('minutesFace');
-const secondsFace = document.getElementById('secondsFace');
 
-// Get initial start time from user (the initial values are stored as constants in case the user wants to reset the timer)
-const hours = parseInt(prompt('How many HOURS do you want to add to the timer?'));
-const minutes = parseInt(prompt('How many MINUTES do you want to add to the timer?'));
-const seconds = parseInt(prompt('How many SECONDS do you want to add to the timer?'));
-
-let totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
-let hoursRemaining, minutesRemaining, secondsRemaining;
-let invokeInterval = null; // this is where window.setInterval() will be stored. It needs to be global so I can toggle between 'Start' and 'Pause'
-
-// Set the initial start time on the clock 
-calculateTimeRemaining();
-drawClockFace();
-
-// Click the start button to start the countdown
-startButton.addEventListener( 'click', toggleTimer );
+// Event Listeners
+keys.forEach(function (key) {
+  key.addEventListener("click", timerInput);
+});
+startButton.addEventListener( 'click', startPause );
 resetButton.addEventListener( 'click', reset );
 
-function toggleTimer() {
+// Functions
+function timerInput(e) {
+  if (counter === 6) {
+    keys.forEach(function (key) {
+      key.removeEventListener("click", timerInput);
+    });
+  }
+  clockArray.push(e.target.innerText);
+  clockArray.shift();
+  timeStr = '';
+  clockArray.forEach(function (digit, index) {
+    timeStr += digit;
+    if (index === 1) {
+      timeStr += "h";
+    }
+    if (index === 3) {
+      timeStr += "m";
+    }
+    if (index === 5) {
+      timeStr += "s";
+    }
+  });
+  drawClock();
+  counter++;
+}
+
+function startPause() {
   if (!invokeInterval) { // this block starts the timer
+    strToNums();
     invokeInterval = window.setInterval( tick, 1000 );
     startButton.innerHTML = 'Pause';
   } else { // this block pauses the timer
@@ -36,84 +58,50 @@ function toggleTimer() {
   }
 }
 
+function reset() {
+  window.clearInterval(invokeInterval);
+  invokeInterval = null;
+  counter = 1;
+  keys.forEach(function (key) {
+      key.addEventListener("click", timerInput);
+    });
+  clockArray = [0,0,0,0,0,0];
+  timeStr = `00h00m00s`;
+  drawClock();
+  startButton.innerHTML = 'Start';
+}
+
+function strToNums() {
+  hrs = parseInt(timeStr[0] + timeStr[1]) * 3600;
+  mins = parseInt(timeStr[3] + timeStr[4]) * 60;
+  secs = parseInt(timeStr[6] + timeStr[7]);
+  totalSecs = hrs + mins + secs;
+}
+
 function tick() {
-  if (totalSeconds === 0) {
-    endTimer();
+  if (totalSecs === 0) {
+    window.clearInterval(invokeInterval);
   } else {
     removeOneSecond();
   }
 }
 
-function endTimer() {
-  window.clearInterval(invokeInterval); // stops the recurring calls to tick()
-  // View
-  const timesUp = document.getElementById('timesUp');
-  timesUp.innerHTML = "Time's up!";
-}
-
 function removeOneSecond() {
-  totalSeconds--;
-  calculateTimeRemaining();
-  drawClockFace();
+  totalSecs--;
+  numsToStr();
+  drawClock();
 }
 
-function calculateTimeRemaining() {
-  hoursRemaining = Math.floor( totalSeconds / 3600 );
-  minutesRemaining = Math.floor( (totalSeconds % 3600) / 60 );
-  secondsRemaining = ( totalSeconds % 3600 ) % 60;
+function numsToStr() {
+  hrs = Math.floor( totalSecs / 3600 );
+  mins = Math.floor( (totalSecs % 3600) / 60 );
+  secs = ( totalSecs % 3600 ) % 60;
+  timeStr = `${hrs < 10 ? '0' + hrs : hrs}h${mins < 10 ? '0' + mins : mins}m${secs < 10 ? '0' + secs: secs}s`;
 }
 
-function drawClockFace() {
-  hoursFace.innerHTML = `${hoursRemaining < 10 ? '0' : ''}${hoursRemaining}`;
-  minutesFace.innerHTML = `${minutesRemaining < 10 ? '0' : ''}${minutesRemaining}`;
-  secondsFace.innerHTML = `${secondsRemaining < 10 ? '0' : ''}${secondsRemaining}`;
+function drawClock() {
+  clockFace.innerHTML = timeStr;
 }
 
-function reset() {
-  window.clearInterval(invokeInterval);
-  invokeInterval = null;
-  totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
-  calculateTimeRemaining();
-  drawClockFace();
-  startButton.innerHTML = 'Start';
-}
 
-/***********************
- * Edit Timer Board
- * ********************/
- 
-const clockArray = [0, 0, 0, 0, 0, 0];
-const clockFace = document.getElementById("clockFace");
-let clockString = "00h00m00s";
-const keys = document.querySelectorAll("#keyPad > .row > .key");
-keys.forEach(function (key) {
-  key.addEventListener("click", populateTimer);
-});
-let counter = 1;
-
-function populateTimer(e) {
-  if (counter === 6) {
-    keys.forEach(function (key) {
-      key.removeEventListener("click", populateTimer);
-    });
-  }
-  const digitInputted = e.target.innerText;
-  clockArray.push(digitInputted);
-  clockArray.shift();
-  clockString = "";
-  clockArray.forEach(function (digit, index) {
-    clockString += digit;
-    if (index === 1) {
-      clockString += "<span>h </span> ";
-    }
-    if (index === 3) {
-      clockString += "<span>m </span> ";
-    }
-    if (index === 5) {
-      clockString += "<span>s </span> ";
-    }
-  });
-  clockFace.innerHTML = clockString;
-  counter++;
-}
 
